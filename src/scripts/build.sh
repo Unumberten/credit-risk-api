3b42dc092e94aff76fe92f606f91fde1f2100c8e
Unumberten <honestgceba@gmail.com>
2024-10-27 10:49:17 +0200

    Update build.sh

--- src/scripts/build.sh
+++ src/scripts/build.sh
@@ -6,7 +6,11 @@ set -o errexit
 pip install -r requirements.txt
 
 # Convert static asset files
-python manage.py collectstatic --no-input
+python manage.py 
 
 # Apply any outstanding database migrations
 python manage.py migrate
+
+python manage.py createsuperuser
+
+python manage.py runserver
